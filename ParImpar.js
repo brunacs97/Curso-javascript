e5026f392ef48593a85d6f8bd729cf039f0cae8b
bruna <bruna.silva@compasso.com.br>
2023-03-27 11:42:36 -0300

    Exibir valor se é PAR ou IMPAR

---
+++ ParImpar.js
@@ -0,0 +1,21 @@
+//recebe uma quantidade de valores e avalia se é par ou impar
+
+exibirTipo(5);
+
+function exibirTipo(valor){
+    for(let i=0; i <=valor; i++){
+        if (i % 2 ===0)
+            console.log(i, 'PAR');
+        else 
+            console.log(i, 'IMPAR');
+        }
+    }
+
+
+
+
+
+
+
+
+
