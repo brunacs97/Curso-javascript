7ed4b0ff2e8619328dd63cf201864f797c2f7a3e
bruna <bruna.silva@compasso.com.br>
2023-03-27 14:04:43 -0300

    Função calcula media dos alunos

---
+++ mediaEscolar.js
@@ -0,0 +1,26 @@
+//Obter a média a partir de um array
+
+const array = [70,70,80]
+
+console.log(mediadoaluno(array));
+
+function mediadoaluno(notas){
+    const media = calcularMedia(notas)
+    
+    if(media < 59) return 'F';
+    if(media < 69) return 'D';
+    if(media < 79) return 'C';
+    if(media < 89) return 'B';
+    return 'A'
+
+   
+
+}
+
+function calcularMedia(array){
+    let soma =0;
+    for (let valor of array){
+        soma +=valor;
+    }
+    return (soma/array.length);
+}
