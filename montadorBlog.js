abd7ed7e71439cee6622803a001d7865a54f1c66
bruna <bruna.silva@compasso.com.br>
2023-03-28 13:14:35 -0300

    Criando um objeto com array

---
+++ montadorBlog.js
@@ -0,0 +1,14 @@
+let postagem = {
+    titulo: 'teste',
+    mensagem:'mensagem de teste',
+    autor:'autor de teste',
+    vizualizacoes:85,
+    comentarios:[
+        { autor : 'teste autor comentario', mensagem:'b'},
+        { autor : 'teste autor comentario', mensagem:'b'}
+
+    ],
+    estaAoVivo : true
+
+}
+console.log(postagem)
