7e636f391fa58b8d367de201548fa6693fb042fe
bruna <bruna.silva@compasso.com.br>
2023-03-28 13:24:55 -0300

    funcao construtora

--- montadorBlog.js
+++ montadorBlog.js
@@ -1,14 +1,29 @@
-let postagem = {
-    titulo: 'teste',
-    mensagem:'mensagem de teste',
-    autor:'autor de teste',
-    vizualizacoes:85,
-    comentarios:[
-        { autor : 'teste autor comentario', mensagem:'b'},
-        { autor : 'teste autor comentario', mensagem:'b'}
+// //objeto com array
 
-    ],
-    estaAoVivo : true
+// let postagem = {
+//     titulo: 'teste',
+//     mensagem:'mensagem de teste',
+//     autor:'autor de teste',
+//     vizualizacoes:85,
+//     comentarios:[
+//         { autor : 'teste autor comentario', mensagem:'b'},
+//         { autor : 'teste autor comentario', mensagem:'b'}
 
-}
-console.log(postagem)
+//     ],
+//     estaAoVivo : true
+
+// }
+// console.log(postagem)
+
+//criar uma constructor function  que retorna um objeto com array
+function Postagem(titulo,mensagem,autor){
+    this.titulo = titulo,
+    this.mensagem = mensagem,
+    this.autor = autor,
+    visualizacoes = 0 ,
+    this.comentarios  = [],
+    this.estaAoVivo = false
+}   
+
+let postagem = new Postagem ('a','b','c');
+console.log(postagem);
