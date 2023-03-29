61c0d2e04260e14ad3605c8eeba960deb33b6fa2
bruna <bruna.silva@compasso.com.br>
2023-03-29 14:20:28 -0300

    Adicionando variavel nome ao retorno do texto

--- segurancaVirtual.js
+++ segurancaVirtual.js
@@ -1,8 +1,10 @@
 function segurancaVirtual() {
+    nome = document.querySelector('#nome').value;
     nomeConvidado = document.querySelector('#nomeConvidado').value;
+    
     permissaoFesta = ['Bruna', 'Alaim', 'Bianca'];
     if (permissaoFesta.includes(nomeConvidado)) {
-        document.getElementById('PermissaoEntrada').innerText = 'Você pode entrar'
+        document.getElementById('PermissaoEntrada').innerText = `Você pode entrar ${nome}`
     }
     else {
         document.getElementById('PermissaoEntrada').innerText = 'Você não pode entrar'
