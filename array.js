d2cbfc34b674323646d63768adc739004cd89829
bruna <bruna.silva@compasso.com.br>
2023-03-29 10:33:00 -0300

    Combinando Arrays

---
+++ array.js
@@ -0,0 +1,14 @@
+const numero = [1,2,3,4];
+
+//usando join
+const combinado = numeros.join('.');
+console.log(combinado);
+
+//usando split
+const frase = "Ola bem vindo!";
+const fraseSeparada = frase.split(' ');
+console.log(fraseSeparada);
+
+//usando join 
+
+console.log(fraseSeparada.join('-'));
