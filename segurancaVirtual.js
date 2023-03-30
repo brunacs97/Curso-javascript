0bcd00aa1d8d95ff1e45c2351703740634ee639e
bruna <bruna.silva@compasso.com.br>
2023-03-30 08:54:47 -0300

    adição de mascara para aceitar apenas letras

--- segurancaVirtual.js
+++ segurancaVirtual.js
@@ -1,22 +1,31 @@
 function segurancaVirtual() {
-    nome = document.querySelector('#nome').value;
+    nome2 = document.querySelector('#nome').value;
     nomeConvidado = document.querySelector('#nomeConvidado').value;
     
     permissaoFesta = ['Bruna', 'Alaim', 'Bianca'];
 
-    if (nome === '') {
+    //não funciona pq todo campo do tipo text retorna string usar regex abaixo da funcao myfunction()
+    // if (typeof nome.value !== 'string' || typeof nomeConvidado.value !== 'string') {
+    //     alert('Digite um texto')
+    // }
+
+    if (nome2 === '') {
         alert('Digite seu nome')
     }
     if (nomeConvidado === '') {
         alert('Digite nome convidado')
     }
     if (permissaoFesta.includes(nomeConvidado)) {
-        document.getElementById('PermissaoEntrada').innerText = `Você pode entrar ${nome}`
+        document.getElementById('PermissaoEntrada').innerText = `Você pode entrar ${nome2}`
     }
     else {
         document.getElementById('PermissaoEntrada').innerText = 'Você não pode entrar'
     }
 }
+
+function apenasLetras(evt) {
+    if(!/[a-záéíóúàèìòùãõâêîôûäëïöüç]/i.test(evt.key)) return false;
+  }
 
 
 
